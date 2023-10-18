b3ea3435b96a04f8c91ddd2bf05dd5123a71a31d
Yesh <104682668+yesheb100@users.noreply.github.com>
2023-10-19 01:12:09 +0500

    Update Increment_matrix.py

--- Increment_matrix.py
+++ Increment_matrix.py
@@ -15,7 +15,7 @@ for i in range(1, 101):
     else:
         current_number += 10  # Add 10 to the current number
 
-''' required output no limit on coloumns:
+''' required output:
 1 11 21 31 41 51 61 71 81 91 
 2 12 22 32 42 52 62 72 82 92 
 3 13 23 33 43 53 63 73 83 93 
